1031a567947628f231779ac42b4405e5f9d57476
Ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-03-16 15:30:14 -0700

    refactor command deployment

--- src/utils/deployCommands.ts
+++ src/utils/deployCommands.ts
@@ -2,32 +2,26 @@ import { REST, Routes, Client } from 'discord.js';
 
 const deployCommands = async (client: Client) => {
   if (!client.user) {
-    console.log(`Client missing user properties, cancelling command deployment...`);
+    console.error('Client missing user properties, cancelling command deployment.');
     return;
   }
 
   const rest = new REST().setToken(process.env.DISCORD_TOKEN);
-  const commandsData = client.commands.map((command) => command.data.toJSON());
+  const commandsData = client.commands.map(command => command.data.toJSON());
+
+  const route =
+    process.env.NODE_ENV === 'production'
+      ? Routes.applicationCommands(client.user.id)
+      : Routes.applicationGuildCommands(client.user.id, process.env.DEV_GUILD_ID);
 
   try {
-    if (process.env.NODE_ENV === 'production') {
-      await rest.put(Routes.applicationCommands(client.user.id), {
-        body: commandsData,
-      });
+    await rest.put(route, { body: [] });
+    console.log('Application (/) commands successfully refreshed.');
 
-      console.log('Application (/) commands successfully registered to all guilds.');
-    } else {
-      await rest.put(
-        Routes.applicationGuildCommands(client.user.id, process.env.DEV_GUILD_ID),
-        { body: commandsData },
-      );
-
-      console.log(
-        'Application (/) commands successfully registered to development guild.',
-      );
-    }
+    await rest.put(route, { body: commandsData });
+    console.log('Application (/) commands successfully registered.');
   } catch (error) {
-    console.error(`Error registering application (/) commands: ${error}`);
+    console.error(`Error registering (/) commands: ${error}`);
   }
 };
 
