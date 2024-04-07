4ff99868ec0512b17b2d75a97cf416cc1fae5870
Ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-04-07 11:16:11 -0700

    start creating new message records

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,13 +1,31 @@
 import { createEvent } from '../interfaces/applicationEvent';
 
-const messageCreate = createEvent('messageCreate', false, (prisma, message) => {
-  if (!message.guild || !message.channel) return;
-  if (message.author.bot || message.system) return;
+const messageCreate = createEvent(
+  'messageCreate',
+  false,
+  async (prisma, message) => {
+    if (!message.guild || !message.channel) return;
+    if (message.author.bot || message.system) return;
 
-  const startsWithCommandCharacter = /^[!\/?]/i;
-  if (startsWithCommandCharacter.test(message.content)) {
-    return;
-  }
-});
+    const startsWithCommandCharacter = /^[!\/?]/i;
+    if (startsWithCommandCharacter.test(message.content)) return;
+    if (message.content.split(' ').length < 2) return;
+
+    const isWatchChannel = await prisma.channel.findUnique({
+      where: { channelId: message.channelId },
+    });
+    if (!isWatchChannel) return;
+
+    await prisma.message.create({
+      data: {
+        content: message.content,
+        messageId: message.id,
+        channel: {
+          connect: { id: isWatchChannel.id },
+        },
+      },
+    });
+  },
+);
 
 export default messageCreate;
