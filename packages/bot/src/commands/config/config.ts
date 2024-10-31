1b47fe0df61d59f78492f6ce08f3f8a3323312df
ohmrr <89240639+ohmrr@users.noreply.github.com>
2024-10-31 04:54:25 +0000

    fix missing break causing error

--- packages/bot/src/commands/config/config.ts
+++ packages/bot/src/commands/config/config.ts
@@ -59,6 +59,7 @@ const config: Command = {
         break;
       case 'view':
         await view.execute(interaction);
+        break;
       case 'resetlog':
         await resetlog.execute(interaction);
         break;
