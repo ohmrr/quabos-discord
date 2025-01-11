243ed3ca97d242f263ec2210fd3706288d48ded5
ohmrr <89240639+ohmrr@users.noreply.github.com>
2025-01-11 20:09:50 +0000

    Container component accepts a tailwind bgColorClass, padding, and maxWidth prop

---
+++ packages/website/src/components/Container.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+export interface ContainerProps {
+  bgColorClass?: string;
+  padding?: number;
+  maxWidth?: number;
+  children?: ReactNode;
+}
+
+export default function Container({
+  bgColorClass,
+  padding = 0,
+  maxWidth = 1200,
+  children,
+}: ContainerProps) {
+  return (
+    <div className={`h-full ${bgColorClass}`} style={{ padding: `0 ${padding}px`}}>
+      <div className="mx-auto h-full" style={{ maxWidth: `${maxWidth}px` }}>
+        {children}
+      </div>
+    </div>
+  );
+}
