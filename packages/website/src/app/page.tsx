96dc8e786633ec94415585d632812e8615fe73ca
ohmrr <89240639+ohmrr@users.noreply.github.com>
2025-01-14 10:23:05 +0000

    changes to Container.tsx, more props and ability to add tailwind classes and tailwind bg color class that spans entire screen

--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -1,17 +1,29 @@
-import StarBackground from '@/components/StarBackground';
+import Container from '@/components/Container';
+import StarBackground from '@/components/HomePage/StarBackground';
 
 export default function Home() {
   return (
-    <div className='relative w-full h-screen'>
-      <div className='absolute inset-0 w-full h-full'>
+    <div className='relative h-screen w-screen'>
+      <div className='absolute inset-0'>
         <StarBackground />
       </div>
 
-      <div className='absolute inset-0 flex items-center justify-center'>
-        <div className='flex flex-col items-center justify-center'>
-          <h1 className='text-6xl text-center font-bold'>Quabos</h1>
+      <Container className='absolute inset-0 flex items-center justify-center'>
+        <div className='flex flex-col items-center justify-center gap-y-10'>
+          <h1 className='text-6xl font-bold text-center'>Quabos</h1>
+
+          <p className='text-2xl text-gray-300 font-norma'>Discord Bot from Hell.</p>
+
+          <a
+            className='border px-5 py-2 font-semibold bg-neutral-800 hover:bg-neutral-900 transition-colors duration-300 rounded-lg shadow-md'
+            href='https://github.com/ohmrr/quabos-discord'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            GitHub
+          </a>
         </div>
-      </div>
+      </Container>
     </div>
   );
 }
--- packages/website/src/components/Container.tsx
+++ packages/website/src/components/Container.tsx
@@ -2,20 +2,22 @@ import { ReactNode } from 'react';
 
 interface ContainerProps {
   bgColorClass?: string;
+  className?: string;
   padding?: number;
   maxWidth?: number;
   children?: ReactNode;
 }
 
 export default function Container({
-  bgColorClass,
+  bgColorClass = '',
+  className = '',
   padding = 0,
   maxWidth = 1200,
   children,
 }: ContainerProps) {
   return (
     <div className={`h-full ${bgColorClass}`} style={{ padding: `0 ${padding}px` }}>
-      <div className='mx-auto h-full' style={{ maxWidth: `${maxWidth}px` }}>
+      <div className={`mx-auto ${className}`} style={{ maxWidth: `${maxWidth}px` }}>
         {children}
       </div>
     </div>
