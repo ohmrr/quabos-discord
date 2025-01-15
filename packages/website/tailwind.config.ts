beddfd70ec03bfb9a75725a76515fcadd5767a3b
ohmrr <89240639+ohmrr@users.noreply.github.com>
2025-01-15 00:56:12 +0000

    add prettier tailwind plugin

--- packages/website/tailwind.config.ts
+++ packages/website/tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      fontFamily: {
+        header: ['Josefin Sans', 'serif'],
+        sans: ['Outfit', 'sans-serif']
+      }
     },
   },
   plugins: [],
