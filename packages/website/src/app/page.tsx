89ca435b141f1e9a7b0b7ae77372cc6a0441ea61
ohmrr <89240639+ohmrr@users.noreply.github.com>
2025-01-15 01:32:58 +0000

    button redesign, WIP background image. remove unnecessary svgs

--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -5,39 +5,20 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='relative h-screen w-screen'>
+    <div className='main relative h-screen w-screen'>
       <div className='absolute inset-0'>
         <StarBackground />
       </div>
 
-      <Container className='absolute px-[20px] inset-0 flex items-center justify-center'>
-        <div className='flex flex-col items-center justify-center gap-y-10'>
-          <h1 className='text-center text-5xl font-bold md:text-7xl'>Quabos</h1>
+      <Container className='absolute inset-0 flex items-center justify-center px-4 md:px-10'>
+        <div className='flex flex-col items-center justify-center gap-y-8 text-center'>
+          <h1 className='text-center text-6xl font-bold tracking-tight md:text-8xl'>Quabos</h1>
 
-          <p className='text-xl font-normal text-center md:text-2xl md:max-w-[75%]'>An entertainment focused Discord bot that uses Markov chains to generate its own messages.</p>
+          <p className='text-lg text-gray-300 md:text-xl max-w-3xl'>An entertainment focused Discord bot that uses Markov chains to generate its own messages.</p>
 
-          <div className="flex flex-row gap-x-8">
-          <a
-            className='rounded-lg border bg-neutral-800 px-5 py-2 font-semibold shadow-md transition-colors duration-300 hover:bg-neutral-900'
-            href='https://github.com/ohmrr/quabos-discord'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <div className="flex flex-row items-center justify-center gap-x-4">
-            GitHub
-            <Icon icon="mdi:github" height="32px" width="32px" />
-            </div>
-          </a>
-
-          <Link
-            className='rounded-lg border bg-blue-800 px-5 py-2 font-semibold shadow-md transition-colors duration-300 hover:bg-blue-900'
-            href='/'
-          >
-            <div className="flex flex-row items-center justify-center gap-x-4">
-            Learn More
-            <Icon icon="mdi:external-link" height="32px" width="32px" />
-            </div>
-          </Link>
+          <div className="flex flex-row gap-x-6 mt-6">
+            <Link className="text-base flex items-center gap-x-3 rounded-lg shadow-md border transition duration-300 hover:bg-blue-900 border-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-blue-800 px-6 py-3 font-semibold" href="/"><Icon icon="mdi:external-link" height="24px" width="24px" /> Guide</Link>
+            <a className="text-base flex items-center gap-x-3 rounded-lg shadow-md border transition duration-300 hover:bg-neutral-900 border-neutral-700 focus:outline-none focus:ring-2 focus:ring-neutral-500 bg-neutral-800 px-6 py-3 font-semibold" href="https://github.com/ohmrr/quabos-discord" target="_blank" rel="noopener noreferrer"> <Icon icon="mdi:external-link" height="24px" width="24px" />GitHub</a>
           </div>
 
         </div>
